fb17919428e7d8430fcf394a7cf4feff67b45c31
undrftd <hhmanongsong@gmail.com>
2016-12-18 21:13:58 +0800

    minor

--- application/controllers/Admin_Accounts.php
+++ application/controllers/Admin_Accounts.php
@@ -9,6 +9,36 @@ class Admin_Accounts extends MY_Controller {
     	$data['admin'] = $this->model_accounts->get_admin();
     	$data['deact'] = $this->model_accounts->get_deact();
     	$data['main_content'] = 'view_adminaccounts';
-		$this->load->view('includes/adaccounts_template', $data);
+		$this->load->view('includes/admin_accounts_template', $data);
+    }
+
+    function adduser()
+    {
+    	$data['main_content'] = 'view_adminaddaccounts';
+		$this->load->view('includes/admin_addaccount_template', $data);	
+    	$this->load->model('model_accounts');	
+	}	
+
+	function createuser()
+    {
+    	$this->load->model('model_accounts');
+
+    	if($query = $this->model_accounts->create_account()) {
+				
+				redirect('admin_accounts');
     }
 }
+    
+ 
+	
+	/*function acc_deactivate()
+    {
+    	if(isset($_GET['userid']))
+    	{
+        	$id=$_GET['userid'];
+        	$this->load->model('model_accounts');
+        	$this->model_accounts->deactivate($id);
+       		redirect('admin_accounts');
+    	}
+	}*/
+}	
