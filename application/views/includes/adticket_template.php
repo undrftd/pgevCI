8c5743ec1b3dbad64d55ff836a5cf63e96b2b455
undrftd <hhmanongsong@gmail.com>
2016-12-22 17:18:14 +0800

    a

--- application/views/includes/adticket_template.php
+++
@@ -1,7 +0,0 @@
-<?php $this->load->view('includes/adticket_header'); ?>
-
-<?php $this->load->view('includes/admin_navigation'); ?>
-
-<?php $this->load->view($main_content); ?>
-
-<?php $this->load->view('includes/adticket_footer'); ?>
