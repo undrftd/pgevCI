0f8513c185f760b5db231a948859f777909c16be
mhmmmarc <marcmaliswag@gmail.com>
2017-01-11 11:53:11 +0800

    Front-end v17

--- public/js/main.js
+++ public/js/main.js
@@ -25,3 +25,27 @@ $(document).ready(function(){
         $("#saveButton").show();
     });
 });
+
+$(document).ready(function () {
+            //iterate through each textboxes and add keyup
+            //handler to trigger sum event
+            $(".txt").each(function () {
+                $(this).keyup(function () {
+                    calculateSum();
+                });
+            });
+        });
+
+        function calculateSum() {
+            var sum = 0;
+            //iterate through each textboxes and add the values
+            $(".txt").each(function () {
+                //add only if the value is number
+                if (!isNaN($(this).val()) && $(this).val().length != 0) {
+                    sum += parseFloat(this.value);
+                }
+            });
+
+            //.toFixed() method will roundoff the final sum to 2 decimal places
+            $("#sum").val(sum.toFixed(2));
+        }
