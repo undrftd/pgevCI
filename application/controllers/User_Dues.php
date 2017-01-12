d880e29afc1e9bbd086c6d41f551b06f9d5338dd
undrftd <hhmanongsong@gmail.com>
2017-01-12 15:55:18 +0800

    code cleanup

--- application/controllers/User_Dues.php
+++ application/controllers/User_Dues.php
@@ -3,28 +3,11 @@
 class User_Dues extends MY_Controller{
 
 
-          function index()
-          {
-            $this->template->load('user_template', 'view_userdues');
+  function index()
+  {
+    $this->template->load('user_template', 'view_userdues');
 
-          }
-
-
-
-          function total_value($userid)
-          {
-
-          }
-
-
-
-
-
+  }
 
 }
 
-
-
-
-
-?>
