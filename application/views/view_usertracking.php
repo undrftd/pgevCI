f6ce9c0d270e011a210dc385d9f7fd566153116d
mhmmmarc <marcmaliswag@gmail.com>
2017-01-22 23:18:42 +0800

    Front-end v34

--- application/views/view_usertracking.php
+++ application/views/view_usertracking.php
@@ -6,19 +6,23 @@
         <div class="header-style">
           <h1> Ticket Tracking </h1>
         </div>
-        
-        <div class="admin-message text-center">
 
-          <p> Note: Below are the last five tickets you have recently submitted. In order to have a detailed history of your ticket submissions, click the "View History" button below.
-          </p>
-
-        </div>
-
-        <br><br>
+        <br>
 
         <div class="row">
 
-          <div class="col-xs-12 col-sm-12 col-md-5 col-md-offset-1">
+          <div class="col-xs-12 col-sm-12 col-md-12">
+
+            <div class="admin-message text-center">
+
+              <p> Note: Below are the last five tickets you have recently submitted. In order to have a detailed history of your ticket submissions, click the "View History" button below. For further concerns, kidnly contact
+                us through the number given below.
+                <p> <strong>Parkwood Greens Executive Village Administrators (Gemille): (02) 887-8888 </strong> </p>
+              </p>
+
+            </div>
+
+            <br>
 
             <div class="table-responsive">
 
@@ -33,57 +37,57 @@
                 <?php foreach($result as $row): ?>
                 <tr>
                     <td><?php echo $row->request_type . "-" . $row->ticketid; ?></td>
-                    <td><?php 
-                              if($row->request_type == 'RGC') 
+                    <td><?php
+                              if($row->request_type == 'RGC')
                               {
-                                echo "Grass Cutting"; 
-                              } 
+                                echo "Grass Cutting";
+                              }
                               else if($row->request_type == 'RTC')
-                              { 
-                                echo "Trash Collection"; 
-                              } 
+                              {
+                                echo "Trash Collection";
+                              }
                               else if($row->request_type == 'RPC')
-                              { 
+                              {
                                 echo "Pest Control";
                               }
                                else if($row->request_type == 'RMP')
-                              { 
-                                echo "Malfunctioning Post Lights"; 
-                              } 
+                              {
+                                echo "Malfunctioning Post Lights";
+                              }
                               else if($row->request_type == 'RPL')
-                              { 
+                              {
                                 echo "Water Pipeline Leakages";
                               }
                                else if($row->request_type == 'RBD')
-                              { 
-                                echo "Blocked Drainage"; 
-                              } 
+                              {
+                                echo "Blocked Drainage";
+                              }
                               else if($row->request_type == 'RSC')
-                              { 
+                              {
                                 echo "Electrical Short Circuit";
                               }
                                else if($row->request_type == 'RMD')
-                              { 
-                                echo "Monthly Dues"; 
-                              } 
+                              {
+                                echo "Monthly Dues";
+                              }
                               else if($row->request_type == 'ROT')
-                              { 
+                              {
                                 echo "Other";
                               }
                                else if($row->request_type == 'EFR')
-                              { 
-                                echo "Fire"; 
-                              } 
+                              {
+                                echo "Fire";
+                              }
                               else if($row->request_type == 'ERB')
-                              { 
+                              {
                                 echo "Robbery";
                               }
                               else if($row->request_type == 'EBT')
-                              { 
-                                echo "Broken House Tube"; 
-                              } 
+                              {
+                                echo "Broken House Tube";
+                              }
                               else if($row->request_type == 'ESP')
-                              { 
+                              {
                                 echo "Suspicious Person";
                               }  ?>
                     </td>
@@ -102,23 +106,6 @@
 
           <div class="clearfix visible-md-block"></div>
           <div class="clearfix visible-sm-block"></div>
-
-          <div class="col-xs-12 col-sm-12 col-md-3 col-md-offset-1">
-
-            <div class="information">
-
-                <div class="form-group">
-
-                  <h4> For further concerns, please contact: </h4>
-                  <input class="form-control" id="sel1" type="text" placeholder="887-8888" readonly>
-
-              </div>
-
-            </div>
-            <br>
-            <br>
-
-          </div>
 
 
         </div>
