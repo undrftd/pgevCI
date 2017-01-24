8e959b9bf3e0f82d021bdf3637efddb32288ce19
undrftd <hhmanongsong@gmail.com>
2017-01-24 21:13:18 +0800

    added closed ticketing

--- application/controllers/Admin_Ticketing.php
+++ application/controllers/Admin_Ticketing.php
@@ -33,13 +33,6 @@ class Admin_Ticketing extends MY_Controller {
     	$this->template->load('admin_template', 'view_adminticketing_new', $data);
     }
 
-    function ticketdetails($ticketid)
-    {
-        $this->model_ticketing->set_timeopened($ticketid);
-    	$data['result'] = $this->model_ticketing->get_newticketdetails($ticketid);
-    	$this->template->load('admin_template', 'view_adminmoretickets', $data);
-    }
-
     function progress_tickets()
     {
         $config['base_url'] = site_url('admin_ticketing/new');
@@ -64,8 +57,45 @@ class Admin_Ticketing extends MY_Controller {
         $this->pagination->initialize($config);
         $data['progressticketlinks'] = $this->pagination->create_links();
 
-        $data['count'] = $this->model_ticketing->count_progresstickets();
+        $data['count'] = $this->model_ticketing->count_newtickets();
         $data['result'] = $this->model_ticketing->get_progresstickets($config['per_page'], $this->uri->segment(3));
         $this->template->load('admin_template', 'view_adminticketing_inprogress', $data);       
     }
+
+    function closed_tickets()
+    {
+        $config['base_url'] = site_url('admin_ticketing/new');
+        $config['total_rows'] = $this->model_ticketing->count_closedtickets();
+        $config['per_page'] =  20;
+        $config['num_links'] = 5;
+        $config['use_page_numbers'] = FALSE;
+        $config['full_tag_open'] = "<ul class='pagination'>";
+        $config['full_tag_close'] ="</ul>";
+        $config['num_tag_open'] = '<li>';   
+        $config['num_tag_close'] = '</li>';
+        $config['cur_tag_open'] = "<li class='disabled'><li class='active'><a>";
+        $config['cur_tag_close'] = "<span class='sr-only'></span></a></li>";
+        $config['next_tag_open'] = "<li>";
+        $config['next_tagl_close'] = "</li>";
+        $config['prev_tag_open'] = "<li>";
+        $config['prev_tagl_close'] = "</li>";
+        $config['first_tag_open'] = "<li>";
+        $config['first_tagl_close'] = "</li>";
+        $config['last_tag_open'] = "<li>";
+        $config['last_tagl_close'] = "</li>";
+        $this->pagination->initialize($config);
+        $data['closedticketlinks'] = $this->pagination->create_links();
+
+        $data['count'] = $this->model_ticketing->count_newtickets();
+        $data['result'] = $this->model_ticketing->get_closedtickets($config['per_page'], $this->uri->segment(3));
+        $this->template->load('admin_template', 'view_adminticketing_closed', $data);       
+    }
+
+    function ticketdetails($ticketid)
+    {
+        $this->model_ticketing->set_timeopened($ticketid);
+        $data['result'] = $this->model_ticketing->get_newticketdetails($ticketid);
+        $this->template->load('admin_template', 'view_adminmoretickets', $data);
+    }
+
 }
