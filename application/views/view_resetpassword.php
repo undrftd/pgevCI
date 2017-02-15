04811c9c454b47819dd349374417208c6e18cef4
mhmmmarc <marcmaliswag@gmail.com>
2017-02-15 21:31:00 +0800

    Front-end v62

--- application/views/view_resetpassword.php
+++ application/views/view_resetpassword.php
@@ -1,5 +1,55 @@
-	Enter Email:
-	<form action="<?php echo site_url()?>login/resetpassword_validation" method="POST">
-	<input type="email" name="email" placeholder="Enter email:">
-	<input type="submit">
-	
+<br>
+
+<div class="user-deact">
+
+  <div class="row">
+
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+
+			<?php if ($this->session->flashdata('profilefeedback')){ ?>
+				<div class="success-message text-center" id="prompt-message">
+					<h3> Hello, <?php echo $this->session->userdata('firstname');?>. </h3>
+					<p> <?php echo $this->session->flashdata('profilefeedback'); ?>  </p><br>
+					<button type="button" class="btn btn-custom-2" id="close-button">Dismiss</button><br><br>
+				</div>
+				<br>
+			<?php } ?>
+
+      <div class="announcement-message">
+
+        <h4>Forgot your password?</h4><hr>
+
+				<p>Enter your email address</p>
+
+				<form action="<?php echo site_url()?>login/resetpassword_validation" method="POST">
+
+					<input type="email" class="form-control" name="email" id="sel1">
+
+					<br><br>
+
+					<div class="action-buttons text-right">
+
+						<a href="<?php echo base_url(); ?>login/signout"><button type="button" class="btn btn-custom-1">Back to Sign In</button></a>
+
+						<button type="submit" class="btn btn-custom">Submit</button>
+
+					</div>
+
+				</form>
+
+				<hr>
+
+				<div class="table-legend">
+					<p> <strong> <span class="dot-style">&middot;</span>&nbsp;Note: </strong>&nbsp;If you cannot remember your email address too. Kindly contact us through this number: 887-8888
+					</p>
+				</div>
+
+      </div>
+
+      <br><br>
+
+    </div>
+
+  </div>
+
+</div>
