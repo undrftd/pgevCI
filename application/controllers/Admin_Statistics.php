11555dd7f985200fd646d797b0acf6a3cf2c0325
undrftd <hhmanongsong@gmail.com>
2017-02-17 17:02:01 +0800

    Back-end v39

--- application/controllers/Admin_Statistics.php
+++ application/controllers/Admin_Statistics.php
@@ -82,4 +82,73 @@ class Admin_Statistics extends MY_Controller {
     	$this->template->load('admin_template', 'view_adminstatistics', $data);
 	}
 
+    function export_pdf()
+    {
+        $data['countjanuary'] =$this->model_statistics->count_january();
+        $data['countfebruary'] =$this->model_statistics->count_february();
+        $data['countmarch'] =$this->model_statistics->count_march();
+        $data['countapril'] =$this->model_statistics->count_april();
+        $data['countmay'] =$this->model_statistics->count_may();
+        $data['countjune'] =$this->model_statistics->count_june();
+        $data['countjuly'] =$this->model_statistics->count_july();
+        $data['countaugust'] =$this->model_statistics->count_august();
+        $data['countseptember'] =$this->model_statistics->count_september();
+        $data['countoctober'] =$this->model_statistics->count_october();
+        $data['countnovember'] =$this->model_statistics->count_november();
+        $data['countdecember'] =$this->model_statistics->count_december();
+        $data['courtone'] = $this->model_statistics->count_reservationcourtone();
+        $data['courttwo'] = $this->model_statistics->count_reservationcourttwo();
+        $data['clubhouse'] = $this->model_statistics->count_reservationclubhouse();
+        $data['carsticker'] = $this->model_statistics->count_carsticker();
+        $data['workpermit'] = $this->model_statistics->count_workpermit();
+        $data['renovation'] = $this->model_statistics->count_renovation();
+        $data['totaltickets'] = $this->model_statistics->count_totaltickets();
+        $data['totalfire'] = $this->model_statistics->count_totalfire();
+        $data['progressfire'] = $this->model_statistics->count_progressfire();
+        $data['closedfire'] = $this->model_statistics->count_closedfire();
+        $data['totalgrasscutting'] = $this->model_statistics->count_totalgrasscutting();
+        $data['progressgrasscutting'] = $this->model_statistics->count_progressgrasscutting();
+        $data['closedgrasscutting'] = $this->model_statistics->count_closedgrasscutting();
+        $data['totalgarbage'] = $this->model_statistics->count_totalgarbage();
+        $data['progressgarbage'] = $this->model_statistics->count_progressgarbage();
+        $data['closedgarbage'] = $this->model_statistics->count_closedgarbage();
+        $data['totalpest'] = $this->model_statistics->count_totalpest();
+        $data['progresspest'] = $this->model_statistics->count_progresspest();
+        $data['closedpest'] = $this->model_statistics->count_closedpest();
+        $data['totalpost'] = $this->model_statistics->count_totalpost();
+        $data['progresspost'] = $this->model_statistics->count_progresspost();
+        $data['closedpost'] = $this->model_statistics->count_closedpost();
+        $data['totalpipeline'] = $this->model_statistics->count_totalpipeline();
+        $data['progresspipeline'] = $this->model_statistics->count_progresspipeline();
+        $data['closedpipeline'] = $this->model_statistics->count_closedpipeline();
+        $data['totaldrainage'] = $this->model_statistics->count_totaldrainage();
+        $data['progressdrainage'] = $this->model_statistics->count_progressdrainage();
+        $data['closeddrainage'] = $this->model_statistics->count_closeddrainage();
+        $data['totalshortcircuit'] = $this->model_statistics->count_totalshortcircuit();
+        $data['progressshortcircuit'] = $this->model_statistics->count_progressshortcircuit();
+        $data['closedshortcircuit'] = $this->model_statistics->count_closedshortcircuit();
+        $data['totalmonthlydues'] = $this->model_statistics->count_totalmonthlydues();
+        $data['progressmonthlydues'] = $this->model_statistics->count_progressmonthlydues();
+        $data['closedmonthlydues'] = $this->model_statistics->count_closedmonthlydues();
+        $data['totalothers'] = $this->model_statistics->count_totalothers();
+        $data['progressothers'] = $this->model_statistics->count_progressothers();
+        $data['closedothers'] = $this->model_statistics->count_closedothers();
+        $data['totalrobbery'] = $this->model_statistics->count_totalrobbery();
+        $data['progressrobbery'] = $this->model_statistics->count_progressrobbery();
+        $data['closedrobbery'] = $this->model_statistics->count_closedrobbery();
+        $data['totalbrokentube'] = $this->model_statistics->count_totalbrokentube();
+        $data['progressbrokentube'] = $this->model_statistics->count_progressbrokentube();
+        $data['closedbrokentube'] = $this->model_statistics->count_closedbrokentube();
+        $data['totalsuspiciousperson'] = $this->model_statistics->count_totalsuspiciousperson();
+        $data['progresssuspiciousperson'] = $this->model_statistics->count_progresssuspiciousperson();
+        $data['closedsuspiciousperson'] = $this->model_statistics->count_closedsuspiciousperson();
+        $data['totalcctv'] = $this->model_statistics->count_totalcctv();
+        $data['progresscctv'] = $this->model_statistics->count_progresscctv();
+        $data['closedcctv'] = $this->model_statistics->count_closedcctv();
+
+        $this->load->library('pdf');
+        $this->pdf->load_view('view_adminstatistics_export', $data);
+        $this->pdf->render();
+        $this->pdf->stream("welcome.pdf");
+    }
 }
---
+++ application/libraries/dompdf/lib/fonts/dompdf_font_family_cache.php
@@ -0,0 +1,109 @@
+<?php return array (
+  'sans-serif' => array(
+    'normal' => DOMPDF_FONT_DIR . 'Helvetica',
+    'bold' => DOMPDF_FONT_DIR . 'Helvetica-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Helvetica-Oblique',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Helvetica-BoldOblique',
+  ),
+  'times' => array(
+    'normal' => DOMPDF_FONT_DIR . 'Times-Roman',
+    'bold' => DOMPDF_FONT_DIR . 'Times-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Times-Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Times-BoldItalic',
+  ),
+  'times-roman' => array(
+    'normal' => DOMPDF_FONT_DIR . 'Times-Roman',
+    'bold' => DOMPDF_FONT_DIR . 'Times-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Times-Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Times-BoldItalic',
+  ),
+  'courier' => array(
+    'normal' => DOMPDF_FONT_DIR . 'Courier',
+    'bold' => DOMPDF_FONT_DIR . 'Courier-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Courier-Oblique',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Courier-BoldOblique',
+  ),
+  'helvetica' => array(
+    'normal' => DOMPDF_FONT_DIR . 'Helvetica',
+    'bold' => DOMPDF_FONT_DIR . 'Helvetica-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Helvetica-Oblique',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Helvetica-BoldOblique',
+  ),
+  'zapfdingbats' => array(
+    'normal' => DOMPDF_FONT_DIR . 'ZapfDingbats',
+    'bold' => DOMPDF_FONT_DIR . 'ZapfDingbats',
+    'italic' => DOMPDF_FONT_DIR . 'ZapfDingbats',
+    'bold_italic' => DOMPDF_FONT_DIR . 'ZapfDingbats',
+  ),
+  'symbol' => array(
+    'normal' => DOMPDF_FONT_DIR . 'Symbol',
+    'bold' => DOMPDF_FONT_DIR . 'Symbol',
+    'italic' => DOMPDF_FONT_DIR . 'Symbol',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Symbol',
+  ),
+  'serif' => array(
+    'normal' => DOMPDF_FONT_DIR . 'Times-Roman',
+    'bold' => DOMPDF_FONT_DIR . 'Times-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Times-Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Times-BoldItalic',
+  ),
+  'monospace' => array(
+    'normal' => DOMPDF_FONT_DIR . 'Courier',
+    'bold' => DOMPDF_FONT_DIR . 'Courier-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Courier-Oblique',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Courier-BoldOblique',
+  ),
+  'fixed' => array(
+    'normal' => DOMPDF_FONT_DIR . 'Courier',
+    'bold' => DOMPDF_FONT_DIR . 'Courier-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Courier-Oblique',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Courier-BoldOblique',
+  ),
+  'dejavu sans' => array(
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSans-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSans-BoldOblique',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSans-Oblique',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSans',
+  ),
+  'dejavu sans light' => array(
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSans-ExtraLight',
+  ),
+  'dejavu sans condensed' => array(
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSansCondensed-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSansCondensed-BoldOblique',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSansCondensed-Oblique',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSansCondensed',
+  ),
+  'dejavu sans mono' => array(
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSansMono-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSansMono-BoldOblique',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSansMono-Oblique',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSansMono',
+  ),
+  'dejavu serif' => array(
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSerif-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSerif-BoldItalic',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSerif-Italic',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSerif',
+  ),
+  'dejavu serif condensed' => array(
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed-BoldItalic',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed-Italic',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed',
+  ),
+  'glyphicons halflings' => array(
+    'normal' => DOMPDF_FONT_DIR . '6ed5261475a72ba92983868bca0014c4',
+  ),
+  'roboto' => array(
+    'normal' => DOMPDF_FONT_DIR . '7a74fc41feb2263dec6d8a446f9b3b2d',
+    'italic' => DOMPDF_FONT_DIR . 'c44d2fc24944ab069f85c8078b584429',
+  ),
+  'rubik' => array(
+    'normal' => DOMPDF_FONT_DIR . '4a19ce1bab2984dc197a1026ddac3c46',
+    'italic' => DOMPDF_FONT_DIR . '169fb57a09783839a93fae9c38863c63',
+  ),
+  'league-gothic' => array(
+    'normal' => DOMPDF_FONT_DIR . 'e461501ea29562e17c925f7ca85338e6',
+  ),
+) ?>
