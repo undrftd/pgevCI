f912ed4381b553f0cf7c0f50eb76a7ad827da628
undrftd <hhmanongsong@gmail.com>
2017-02-17 18:22:12 +0800

    Back-end v40.2

--- application/views/view_adminstatistics_export.php
+++ application/views/view_adminstatistics_export.php
@@ -5,7 +5,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Title Page</title>
+		<title>Parkwood Greens Executive Village - CRM Statistics</title>
 
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>public/css/main.css">
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>public/css/mobile.css">
