ee3fb4365a652574f272ec93fd418830e25c10d8
mhmmmarc <marcmaliswag@gmail.com>
2017-02-24 12:44:16 +0800

    Front-end v72.1
    Missing class

--- application/views/view_adminannouncements_post.php
+++ application/views/view_adminannouncements_post.php
@@ -25,7 +25,7 @@
 
   <div class="row">
 
-    <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2">
+    <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 nopadding">
 
       <div class="header-style">
         <h1> Post an announcement </h1>
