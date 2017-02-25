11636b9f38d731391a1bcb22ccfd09f7b10510f7
undrftd <hhmanongsong@gmail.com>
2017-02-25 13:03:52 +0800

    Back-end v62.1

--- application/views/view_adminreservation_clubhouse.php
+++ application/views/view_adminreservation_clubhouse.php
@@ -126,7 +126,7 @@
 
                     <?php
                     
-                    $tdX = array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+                    $tdX = array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
 
                     // loop through results setting the array switches
                     foreach ($result as $result)
