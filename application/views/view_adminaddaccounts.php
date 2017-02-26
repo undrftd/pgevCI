65aaf1ddee8d81aa2629f8763ca56c062a40e090
undrftd <hhmanongsong@gmail.com>
2017-02-26 12:46:23 +0800

    Back-end v68.8

--- application/views/view_adminaddaccounts.php
+++ application/views/view_adminaddaccounts.php
@@ -57,7 +57,7 @@
           <br>
 
           <p> Username </p>
-          <input name="username" class="form-control" id="sel1" type="text" placeholder="Must contain numbers only" pattern="[0-9]{8,12}" title="Username must include a minimum of 8 numbers only." required>
+          <input name="username" class="form-control" id="sel1" type="text" placeholder="Must contain numbers only" pattern="[0-9]{8,12}" title="Username must include a minimum of 8 and a maximum of 12 numbers only." required>
               <p class="error"><?php echo form_error('username'); ?></p>
           <br>
 
