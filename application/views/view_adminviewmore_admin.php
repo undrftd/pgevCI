f27c16086505f619da7cb64e1968ea4ff7938774
Howell Manongsong <hhmanongsong@gmail.com>
2017-03-24 13:49:33 +0800

    Back-end v118

--- application/views/view_adminviewmore_admin.php
+++ application/views/view_adminviewmore_admin.php
@@ -183,7 +183,7 @@
                 <br>
 
                 <p> Birthdate </p>
-                <input name="birthdate" class="form-control" id="sel1" type="date" placeholder="" pattern="[a-z A-Z ]{2,30}" title="Middle Name must include a minimum of 2 and a maximum of 30 alphabetical characters only." required>
+                <input name="birthdate" class="form-control" id="sel1" type="text" placeholder="" value="<?php echo htmlentities($view->birthdate); ?>" pattern="[a-z A-Z ]{2,30}" title="Middle Name must include a minimum of 2 and a maximum of 30 alphabetical characters only." required>
                 <p class="error"><?php echo form_error('middlemname'); ?></p>
                 <br>
 
